a6628d484bbaab93e00e685a75fb8ad46efa295c
Abdushakur Bashir <abdushakur.m2304203@st.futminna.edu.ng>
2025-08-10 03:29:47 +0100

    deploy mainnet

--- src/frontend/public/canister-ids.js
+++ src/frontend/public/canister-ids.js
@@ -1,6 +1,5 @@
-// Clypr Canister IDs for Local Development
+// Clypr Canister IDs for Production
 window.canisterIds = {
-  backend: 'uxrrr-q7777-77774-qaaaq-cai',
-  frontend: 'u6s2n-gx777-77774-qaaba-cai',
-  internet_identity: 'uzt4z-lp777-77774-qaabq-cai'
+  backend: '5elod-ciaaa-aaaag-aufgq-cai',
+  frontend: '5nif7-uaaaa-aaaag-aufha-cai'
 };
